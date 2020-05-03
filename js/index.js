c35222cf1875de07ccb08c850e9b8d5b68bfb5b7
Andrzej Łuszcz <endriu1297@gmail.com>
2020-05-03 23:10:12 +0200

    render list of categories and set new current category

--- js/index.js
+++ js/index.js
@@ -3,8 +3,41 @@ import Game from './game';
 const categoryBtn = document.querySelector('.list-buttons__change-category');
 const popUp = document.querySelector('.popup');
 const iconClosePopUp = document.querySelector('.popup__exit');
+const categoriesList = document.querySelector('.popup__list');
+const currentCategory = document.querySelector('.header__category span');
 const game = new Game();
 
+// Create list categories
+function renderCategories() {
+  game.categories.map((category) => {
+    const li = document.createElement('li');
+    li.classList.add('popup__item');
+    li.textContent = category.name;
+    categoriesList.appendChild(li);
+  });
+}
+
+// Set new current category
+
+function setCurrentCategory() {
+  const activeCategory = document.querySelector('.popup__item--active');
+  if (activeCategory) activeCategory.classList.remove('popup__item--active');
+
+  game.currentCategory = this.textContent;
+  currentCategory.textContent = this.textContent;
+
+  this.classList.add('popup__item--active');
+}
+
+function listenerCategory() {
+  const categories = document.querySelectorAll('.popup__item');
+
+  categories.forEach((category) => {
+    category.addEventListener('click', setCurrentCategory);
+  });
+}
+
+// Pop up open/closed
 function showPopUp() {
   popUp.style.display = 'block';
 }
@@ -15,3 +48,6 @@ function closePopUp() {
 
 categoryBtn.addEventListener('click', showPopUp);
 iconClosePopUp.addEventListener('click', closePopUp);
+window.addEventListener('DOMContentLoaded', listenerCategory);
+
+renderCategories();
