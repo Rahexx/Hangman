92642de0cd38c2d4f95632293021b0934d51b0b5
Andrzej Łuszcz <endriu1297@gmail.com>
2020-05-04 19:05:05 +0200

    random passwords when set new category and when click on button. Render password on browser

--- js/game.js
+++ js/game.js
@@ -4,7 +4,7 @@ class Game {
       { name: 'programowanie', passwords: ['React', 'Angular', 'Vue', 'Java'] },
       {
         name: 'Harry Potter',
-        passwords: ['Hermiona', 'Hagrin', 'Dumbledore', 'Ginny'],
+        passwords: ['Hermiona', 'Hagrid', 'Dumbledore', 'Ginny'],
       },
       {
         name: 'Gra o tron',
@@ -17,6 +17,51 @@ class Game {
     this.maxNumberMistake = 8;
     this.scor = 0;
     this.averageTime = 0;
+    this.pastPasswords = [];
+  }
+
+  setCurrentPassword(newPassword) {
+    this.currentPassword = newPassword;
+  }
+
+  getCurrentPassword() {
+    return this.currentPassword;
+  }
+
+  setCurrentCategory(category) {
+    this.currentCategory = category;
+  }
+
+  getCurrentCategory() {
+    return this.currentCategory;
+  }
+
+  pushPastPasswords(newPassword) {
+    this.pastPasswords.push(newPassword);
+  }
+
+  setPastPasswords(length) {
+    this.pastPasswords.length = length;
+  }
+
+  getPastPasswords() {
+    return this.pastPasswords;
+  }
+
+  randomPasswords() {
+    this.currentCategory = this.getCurrentCategory();
+    this.indexCurrentCategories = this.categories.findIndex((category) => category.name === this.currentCategory);
+    this.lengthPasswords = this.categories[this.indexCurrentCategories].passwords.length;
+    this.randomIndex = Math.round(Math.random() * (this.lengthPasswords - 1));
+    this.currentPassword = this.categories[this.indexCurrentCategories].passwords[this.randomIndex];
+    this.itWas = this.pastPasswords.findIndex((password) => password === this.currentPassword);
+
+    if (this.itWas === -1) {
+      this.pushPastPasswords(this.currentPassword);
+      this.setCurrentPassword(this.currentPassword);
+    } else {
+      this.randomPasswords();
+    }
   }
 }
 
--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 import Game from './game';
 
 const categoryBtn = document.querySelector('.list-buttons__change-category');
+const randomBtn = document.querySelector('.list-buttons__random');
 const popUp = document.querySelector('.popup');
 const iconClosePopUp = document.querySelector('.popup__exit');
 const categoriesList = document.querySelector('.popup__list');
-const currentCategory = document.querySelector('.header__category span');
+const currentCategoryElem = document.querySelector('.header__category span');
+const passwordElem = document.querySelector('.password');
 const game = new Game();
 
 // Create list categories
@@ -17,16 +19,36 @@ function renderCategories() {
   });
 }
 
-// Set new current category
+// render password element
+
+function renderPassword() {
+  game.randomPasswords();
+  const passwordLetters = [...game.getCurrentPassword()];
+
+  // Create letter
+  const spanArray = passwordLetters.map((letter) => {
+    return `<span class="password__letter">${letter}</span>`;
+  });
+
+  passwordElem.innerHTML = `
+    <p class="password__content">
+      ${spanArray.join('')}
+    </p>
+  `;
+}
 
 function setCurrentCategory() {
   const activeCategory = document.querySelector('.popup__item--active');
+  game.setPastPasswords(0);
+  console.log(game.getPastPasswords());
   if (activeCategory) activeCategory.classList.remove('popup__item--active');
 
-  game.currentCategory = this.textContent;
-  currentCategory.textContent = this.textContent;
+  game.setCurrentCategory(this.textContent);
+  currentCategoryElem.textContent = this.textContent;
 
   this.classList.add('popup__item--active');
+
+  renderPassword();
 }
 
 function listenerCategory() {
@@ -37,7 +59,6 @@ function listenerCategory() {
   });
 }
 
-// Pop up open/closed
 function showPopUp() {
   popUp.style.display = 'block';
 }
@@ -49,5 +70,6 @@ function closePopUp() {
 categoryBtn.addEventListener('click', showPopUp);
 iconClosePopUp.addEventListener('click', closePopUp);
 window.addEventListener('DOMContentLoaded', listenerCategory);
+randomBtn.addEventListener('click', renderPassword);
 
 renderCategories();
